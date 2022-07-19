95a5a9eaebd80de4018264a3c522d9e865fda4d8
AniketGupta1531 <AniketGupta1531@gmail.com>
2022-07-19 23:04:25 +0530

    added datepicker

--- src/Components/DatePickerView.js
+++ src/Components/DatePickerView.js
@@ -16,19 +16,32 @@ export default class DatePickerView extends React.Component {
     this.setState({dateInput: newDate});
     this.props.onDateChange(newDate);
   };
+
+  getGateFormat = () => {
+    let d = this.state.dateInput;
+    let datestring =
+      d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
+    return datestring;
+  };
+
   render() {
     return (
       <View style={styles.mainStyle}>
         <TextInput
           style={styles.input}
-          value={this.state.dateInput.toString()}
+          value={this.getGateFormat()}
           onChangeText={value => console.log('someting chnage')}
         />
         <Pressable
+          style={{
+            width: '15%',
+            alignContent: 'center',
+            alignItems: 'center',
+          }}
           onPress={() => {
             this.props.openDatePicker();
           }}>
-          <FontAwesomeIcon size={44} icon={faCalendarAlt} color={'#74b9ff'} />
+          <FontAwesomeIcon size={40} icon={faCalendarAlt} color={'#74b9ff'} />
         </Pressable>
         <DatePicker
           modal
@@ -55,6 +68,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
   input: {
-    width: '90%',
+    width: '85%',
+    padding: 10,
+    fontSize: 22,
   },
 });
